f10851985f96de0db13a28b0d1486ebb776e2488
jwy2002 <50856876+jwy2002@users.noreply.github.com>
2021-05-17 18:44:14 +0900

    Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CounterContainer from "./containers/CounterContainer";
 
 const App = () => {
     return (
         <div>
-
+            <CounterContainer />
         </div>
     );
 };
