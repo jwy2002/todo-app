17689559c484f7945acee90bcbdde4afa01d3890
Tony <tonyjun2002@gmail.com>
2021-06-01 22:41:56 +0900

    20210601

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './api_sample/App';
-import App from "./App";
-
+// import App from "./App";
+import App from "./bootstrap/App"
 ReactDOM.render(<App />
     , document.getElementById('root'));
